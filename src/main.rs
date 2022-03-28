d6fd17e8579344a73a1f304a6b33e6eea1a4acb8
Kurohe-san <henrihib.mc@gmail.com>
2022-03-28 17:41:29 +0200

    Added comment in main

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ pub mod package;
 pub mod config;
 pub mod prompt;
 
+// Only calls argument parsing
 fn main() {
     args::parse_args();
     //package::Package::load_db_path("test1", &config::Config::load_config()).list_dependencies().iter().for_each(|p| println!("{}", p));
